f0ada1e0a95a2f7b9cd9ac7974b71d9cda5da88f
Obi-Jian <151472900+Obi-Jian@users.noreply.github.com>
2026-03-13 12:46:21 +0100

    Prova 2

--- es5.py
+++ es5.py
@@ -2,8 +2,7 @@
 ampiezza influenzi la qualità del segnale (trovate nella cartella «materiale» alcuni
 esempi di audio).
 1. Importare un file audio.
-2. Estrarre un singolo canale (mono) e normalizzare l'ampiezza nell'intervallo
-[−1,1].
+2. Estrarre un singolo canale (mono) e normalizzare l'ampiezza nell'intervallo [−1,1].
 3. Utilizzare la funzione di quantizzazione implementata nell'esercizio 4 per
 ridurre la risoluzione del segnale originale a 8, 4 e 2 bit.
 Test d'Ascolto e Confronto:
