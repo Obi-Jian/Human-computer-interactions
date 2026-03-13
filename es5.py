118426b2526b28037f76e6b24efaee24f684ac05
Obi-Jian <151472900+Obi-Jian@users.noreply.github.com>
2026-03-13 12:44:10 +0100

    prova commit

---
+++ es5.py
@@ -0,0 +1,12 @@
+""" Utilizzare un frammento audio complesso per studiare come la riduzione dei livelli di
+ampiezza influenzi la qualità del segnale (trovate nella cartella «materiale» alcuni
+esempi di audio).
+1. Importare un file audio.
+2. Estrarre un singolo canale (mono) e normalizzare l'ampiezza nell'intervallo
+[−1,1].
+3. Utilizzare la funzione di quantizzazione implementata nell'esercizio 4 per
+ridurre la risoluzione del segnale originale a 8, 4 e 2 bit.
+Test d'Ascolto e Confronto:
+1. Riprodurre le versioni a risoluzione ridotta. Descrivere come cambia la
+percezione del suono.
+2. Riprodurre il segnale rumore a 2 bit. Cosa si sente? """
